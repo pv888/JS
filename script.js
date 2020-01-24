c3a7f24273766d578a5b0da26a8de3faf6b569de
pv <pv@gmail.com>
2020-01-24 15:06:06 +0500

    v1

---
+++ script.js
@@ -0,0 +1,18 @@
+var rule = ('Ответь на все вопросы');
+alert (rule);
+var name = prompt('Введите имя');
+console.log('Меня зовут = ' + (name));
+var age = +prompt('Введите возраст')
+console.log('Мне = ' + (age));
+var num1 = +prompt('10 + 10 = ?');
+console.log('ответ = ' + (num1) + (' Правильный ответ = 20'));
+var num2 = +prompt('20 - 17 = ?');
+console.log('Ответ = ' + (num2) + (' Правильный ответ = 3'));
+var num3 = +prompt('50 * 2 = ?');
+console.log('Ответ = ' + (num3) + ('Правильный ответ = 25'));
+var num4 = +prompt('20 / 2 = ?');
+console.log('Ответ = ' + (num4) + ('Правильный ответ = 10'));
+var num5 = prompt('15 % 4 = ?');
+console.log('Ответ = ' + (num5) + ('Правильный ответ = 3,75'));
+var rule2 = ('Перейдите в консоль!')
+alert (rule2);
